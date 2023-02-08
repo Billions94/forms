7d242be0d9a28b0261077b81c433b9b17e4caff9
Ejiroghene Alexander Egbedi <E.a.egbedi@gmail.com>
2023-02-08 19:28:34 +0100

    Added function

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -1,5 +1,23 @@
-export const name: string = 'mess'
+interface Form {
+  name: string;
+  placeholder: string;
+}
 
-export function getInputAttributes() {
-  
+interface InputAttributes {
+  value: string;
+  name: string;
+  placeholder: string;
+}
+
+export function getInputAttributes(
+  input: any,
+  forms: Form[]
+): InputAttributes[] {
+  return Object.values(input).map((value, index) => {
+    return {
+      value: String(value),
+      name: forms[index].name,
+      placeholder: forms[index].placeholder,
+    };
+  });
 }
